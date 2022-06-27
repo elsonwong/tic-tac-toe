dd9210e0f6d95b74d68bf2a8a9c01ee02adfd2bb
elsonwong <elson.wong@live.com.sg>
2022-06-27 20:40:55 +0800

    if (checkWin(currentClass))

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 
 const X_CLASS = 'x'
 const CIRCLE_CLASS = 'circle'
+const WINNING_COMBINATIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
 const cellElements = document.querySelectorAll('[data-cell')
 const board = document.getElementById('board')
 let circleTurn
@@ -20,6 +30,9 @@ function handleClick(e) {
     const cell = e.target
     const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
     placeMark(cell, currentClass)
+    if (checkWin(currentClass)) {
+
+    }
     swapTurns()
     setBoardHoverClass()
 }
