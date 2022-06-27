e516d77dc6f83c70e958b22c7312e7c2e8d87fba
elsonwong <elson.wong@live.com.sg>
2022-06-27 20:12:58 +0800

    function swapTurns()

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 
-const X_CLASS = 'x';
-const CIRCLE_CLASS = 'circle';
+const X_CLASS = 'x'
+const CIRCLE_CLASS = 'circle'
 const cellElements = document.querySelectorAll('[data-cell')
 let circleTurn
 
 cellElements.forEach(cell => {
-    cell.addEventListener('click', handleClick, {once:true})
+    cell.addEventListener('click', handleClick, { once:true })
 })
 
 function handleClick(e) {
     const cell = e.target
     const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
     placeMark(cell, currentClass)
+    swapTurns()
 }
 
 function placeMark(cell, currentClass) {
     cell.classList.add(currentClass)
+}
+function swapTurns(){
+    circleTurn= !circleTurn
 }
