baea07213e593223c68ba4de84909726b24dfe78
elsonwong <elson.wong@live.com.sg>
2022-06-27 19:43:23 +0800

    const x_class, const CIRCLE_CLASS

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
+
+const X_CLASS = 'x';
+const CIRCLE_CIRCLE = 'circle';
 const cellElements = document.querySelectorAll('[data-cell')
+let circleTurn
 
 cellElements.forEach(cell => {
     cell.addEventListener('click', handleClick, {once:true})
 })
+
